d6c13ca652eba81e7793e85b138c7296ba74b688
anasalpure <anas.alpure@gmail.com>
2024-01-26 15:11:48 +0100

    add general theme and shared folder

---
+++ src/shared/components/index.ts
@@ -0,0 +1,1 @@
+export { GenericDialog } from "./GenericDialog";
---
+++ src/theme/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+import { palette } from "./palette";
+import typography from "./typography";
+import { shadows } from "./shadows";
+
+export const theme = createTheme({
+  palette,
+  typography,
+  shadows,
+});
